c8fb4c233f319a72c468caa58337b7227bdce187
US\yingtang <yingtang@deloitte.com>
2018-07-17 18:31:30 -0400

    Switch to single sign on and add logout.

--- js/main.ts
+++ js/main.ts
@@ -7,30 +7,36 @@ import Keycloak from 'keycloak-js';
 
 function main(): void {
 
+    let baseUri = window.location.pathname.substring(0, window.location.pathname.indexOf('/nodejs-playground-web'));
+    let LOGIN_URL = baseUri + '/nodejs-playground-ws/login' + "?target=" + encodeURIComponent(window.location.pathname);
+    let LOGOUT_URL = baseUri + '/nodejs-playground-ws/logout' + "?target=" + encodeURIComponent(window.location.pathname);
+
     // initialize keycloak from client installation json from server
     //let keycloak = Keycloak("data/keycloak.json");
 
+    // initialize with simple config
     let keycloak = Keycloak({
-        url: 'http://localhost:9080/auth',
-        realm: 'Test',
+        url: 'http://localhost/auth',
+        realm: 'Playground',
         clientId: 'nodejs-playground-web'
     });
 
     keycloak.init({
-        onLoad: 'login-required',
-        //onLoad: 'check-sso',
+        onLoad: 'check-sso',
         checkLoginIframe: false
     }).success(function (authenticated) {
         if (authenticated) {
-
             console.log("authenticated...");
             console.log(_.camelCase("Test Lodash"));
             d3.json('data/data.json').then((data)=>{
                 console.log(data);
             });
+            //
+            let logoutLink = document.getElementById("logoutLink");
+            logoutLink.setAttribute("href", LOGOUT_URL);
         } else {
             console.log("user not authenticate...");
-            window.location.href = 'error.html';
+            window.location.href = LOGIN_URL;
         }
     }).error(function() {
         console.log("can not initialize keycloak...");
